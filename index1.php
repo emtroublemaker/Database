6233cc7e06659952f16e605d43c341542a08808e
emtroublemaker <83471925+emtroublemaker@users.noreply.github.com>
2021-11-23 15:38:36 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+</head>
+
+<body>
+
+		<h3>Details to be stored</h3>
+
+		<form action="insert.php" method="post">
+			
+			
+<p>
+				<label for="ID">ID:</label>
+				<input type="text" name="ID" id="ID">
+			</p>
+
+
+
+			
+			
+<p>
+				<label for="name">Name:</label>
+				<input type="text" name="name" id="name">
+			</p>
+
+
+
+			
+			
+<p>
+				<label for="password">Password:</label>
+				<input type="text" name="password" id="password">
+			</p>
+
+			<input type="submit" value="Submit">
+		</form>
+</body>
+
+</html>
